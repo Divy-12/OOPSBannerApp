099f5afa417d6064141701d0a830b8f241af7648
Divy-12 <sridiv1503@gmail.com>
2026-03-03 13:01:12 +0530

    pens

---
+++ Student.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Student{
+    public static void main(String[]args){
+      String name="Anuj";
+    int id=765;
+    System.out.println("Name : "+name);
+    System.out.println("ID : "+id);
+    }
+}
---
+++ pens.java
@@ -0,0 +1,8 @@
+class pens{
+    public static void main(String[]args){
+        int pens=14,students=3;
+        int d=pens/students;
+        int nd=pens%students;
+        System.out.println("The Pen Per Student is "+d+" and the remaining pen not distributed is "+nd);
+    }
+}
