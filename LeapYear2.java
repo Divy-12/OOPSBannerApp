6fe920b6846ecbda0edbd254ba613fedc32b9219
Divy-12 <sridiv1503@gmail.com>
2026-03-16 20:50:13 +0530

    LeapYear2

--- LeapYear2.java
+++ LeapYear2.java
@@ -1,16 +1,10 @@
 import java.util.Scanner;
-public class LeapYear {
+public class LeapYear2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter year: ");
         int year = sc.nextInt();
-        if (year < 1582)
-            System.out.println("Not a valid year");
-        else if (year % 400 == 0)
-            System.out.println("Year is a Leap Year");
-        else if (year % 100 == 0)
-            System.out.println("Not a Leap Year");
-        else if (year % 4 == 0)
+        if(year >= 1582 && (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)))
             System.out.println("Year is a Leap Year");
         else
             System.out.println("Not a Leap Year");
